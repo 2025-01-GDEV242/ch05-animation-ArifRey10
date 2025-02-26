715015c0b9482ac4fe6520b493c02f1197804a27
ArifRey10 <g00296272@stu.raritanval.edu>
2025-02-26 13:52:50 -0500

    finished this class

--- BoxBall.java
+++ BoxBall.java
@@ -12,11 +12,41 @@ public class BoxBall
     private final Color color;
     private final int boxX, boxY, boxWidth, boxHeight;
     private Canvas canvas; 
-
     
      public BoxBall(int boxX, int boxY, int boxWidth, int boxHeight, 
      Canvas canvas)
      {
         Random rand = new Random();
-     
+        this.diameter = 15;
+        this.x = rand.nextInt(boxWidth - diameter) + boxX;
+        this.y = rand.nextInt(boxHeight - diameter) + boxY;
+        this.dx = rand.nextInt(5) + 1;
+        this.dy = rand.nextInt(5) + 1;
+        this.color = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        this.boxX = boxX;
+        this.boxY = boxY;
+        this.boxWidth = boxWidth;
+        this.boxHeight = boxHeight;
+        this.canvas = canvas;
+}
+      public void move() {
+        canvas.eraseCircle(x, y, diameter);
+       
+        if (x + dx < boxX || x + dx + diameter > boxX + boxWidth) {
+            dx = -dx;
+        }
+        if (y + dy < boxY || y + dy + diameter > boxY + boxHeight) {
+            dy = -dy;
+        }
+       
+        x += dx;
+        y += dy;
+
+       
+        draw();
+}
+      public void draw() {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(x, y, diameter);
+}
 }
