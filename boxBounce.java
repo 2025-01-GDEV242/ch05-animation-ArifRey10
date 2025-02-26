166a9d3a016aad9ca1ae578e6195fa5717c381a4
ArifRey10 <g00296272@stu.raritanval.edu>
2025-02-26 14:14:06 -0500

    last commit
    finished the code and it worked well

--- boxBounce.java
+++ boxBounce.java
@@ -19,4 +19,36 @@ public class boxBounce
         myCanvas = new Canvas("Box Bounce", 500, 400);
         balls = new ArrayList<>();
      }
+
+
+ public void boxBounce(int numBalls) {
+        myCanvas.setVisible(true);
+
+        // Draw the box
+        myCanvas.setForegroundColor(Color.BLACK);
+        myCanvas.drawLine(boxX, boxY, boxX + boxWidth, boxY); // Top
+        myCanvas.drawLine(boxX, boxY + boxHeight, boxX + boxWidth, boxY + boxHeight); // Bottom
+        myCanvas.drawLine(boxX, boxY, boxX, boxY + boxHeight); // Left
+        myCanvas.drawLine(boxX + boxWidth, boxY, boxX + boxWidth, boxY + boxHeight); // Right
+
+        // Create balls inside the box
+        for (int i = 0; i < numBalls; i++) {
+            balls.add(new BoxBall(boxX, boxY, boxWidth, boxHeight, myCanvas));
+        }
+
+        // Animate the balls
+        while (true) {
+            myCanvas.wait(20); // Small delay
+            for (BoxBall ball : balls) {
+                ball.move();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        boxBounce demo = new boxBounce();
+        demo.boxBounce(5); // Run the simulation with 5 balls
+    }
 }
+
+
